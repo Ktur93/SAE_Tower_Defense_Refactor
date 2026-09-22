f12285c698df36b28f6c53a530665f3831cf322e
Mathis AURENSAN <maurensan@b109-09.iut.univ-paris8.fr>
2026-09-22 16:24:43 +0200

    test

--- src/main/java/universite_paris8/iut/ademir/demo1/Main.java
+++ src/main/java/universite_paris8/iut/ademir/demo1/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    //test
+    //test 1
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("jeu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1835, 1024);
