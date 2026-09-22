6ff2095062b484e44bf11cddcd8790b545289281
AyoubeM <berudevil@gmail.com>
2026-09-22 16:28:19 +0200

    Pokdof

--- src/main/java/universite_paris8/iut/ademir/demo1/Main.java
+++ src/main/java/universite_paris8/iut/ademir/demo1/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
         stage.setResizable(false);
+        //ddd
     }
 
     public static void main(String[] args) {
